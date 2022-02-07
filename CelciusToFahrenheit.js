ff812cf6b98cd456c0b49cbf286c90508369b03d
webgurusam <shyamghosh666@gmail.com>
2022-02-07 16:42:18 +0600

    Converted Celcius in Fahrenheit by JavaScript

--- CelciusToFahrenheit.js
+++ CelciusToFahrenheit.js
@@ -3,9 +3,9 @@ function getCelcius(celcius) {
     const getFahrenheit = (celcius * 9 / 5) + 32;
     return getFahrenheit;
 }
-const inputCelcius = 10;
+const inputCelcius = 2;
 const getFahrenheitOutput = getCelcius(inputCelcius);
-console.log(getFahrenheitOutput);
+console.log(getFahrenheitOutput, 'Fahrenheit in', inputCelcius, 'degree Celcius');
 
 
 
